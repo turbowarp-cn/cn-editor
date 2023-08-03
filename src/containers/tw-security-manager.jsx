10171d4ca3e156260491958907a5ffb530fe809d
-SIPC- <92251518+SIPC@users.noreply.github.com>
2023-08-03 19:11:27 +0800

    更新 tw-security-manager.jsx

--- src/containers/tw-security-manager.jsx
+++ src/containers/tw-security-manager.jsx
@@ -26,6 +26,7 @@ const isTrustedExtension = url => (
     url.startsWith('https://extensions.turbowarp.cn/') ||
     // For development.
     url.startsWith('http://localhost:8000/') ||
+    url.startsWith('http://localhost:5500/') ||
 
     extensionsTrustedByUser.has(url)
 );
